1f3ab64f02793e4c283de6a199b529d37c3f8fea
Simon <SchwSimon@users.noreply.github.com>
2017-11-03 14:37:55 +0100

    Updated to ES6+

--- LList.js
+++ LList.js
@@ -1,262 +1,284 @@
-(function( global, factory ) {
-	
+(function(global, factory) {
+
 	"use strict";
 
-	if ( typeof module === "object" && typeof module.exports === "object" ) {
+	if (typeof module === "object" && typeof module.exports === "object") {
 		// export as module for environments like NodeJS
-		module.exports = factory( null, true );
+		module.exports = factory(null, true);
 	} else {
-		factory( global, false );
+		factory(global, false);
 	}
-	
-})( ( typeof window !== "undefined" ) ? window : this, function( global, noGlobal ) {
-	
+
+})((typeof window !== "undefined") ? window : this, function(global, noGlobal) {
+
 	"use strict";
-	
-	/*
-	 * Create a new node structure
-	 * @param {*} data The node data
-	 * @return {Object}
-	 */
-	function createNode( data ) {
-		return {
-			data: data || null,
-			next: null,
-			prev: null
-		};
-	}
-	
-	// LList constructor
-	var LList = function() {
-		this.head = null;
-		this.count = 0;
-	};
-	
-	/*
-	 * Iterates over the complete list
-	 * @param {Function} callFunc The function to be called for each node in the list
-	 * * INFO: if callFunc returns non undefined the loop will break and the return value will be returned!
-	 * * @this Will be the same list context reference
-	 * * @args {Object} The current node
-	 * * @args {Number} The current node's list index
-	 */
-	LList.prototype.forEach = function( callFunc ) {
-		if ( !this.head ) return;
-		var node = this.head;
-		var index = 0;
-		while( node ) {
-			var returnValue = callFunc.call( this, node, index++ );
-			if ( typeof returnValue !== "undefined" ) return returnValue;	// return 
-			node = node.next;
-		}
-	};
-	
-	/*
-	 * Get the last node in the list
-	 * @return {Object|Null} The last node in the list or null
-	 */
-	LList.prototype.getLastNode = function() {
-		if ( !this.head ) return null;
-		if ( !this.head.next ) return this.head;
-		var node = this.head.next;
-		while( node.next ) {
-			node = node.next;
-		}
-		return node;
-	};
-	
-	/*
-	 * Adds a node at the end of the list
-	 * @param {*} data The node data
-	 */
-	LList.prototype.push = function( data ) {
-		var node = createNode( data );
-		if ( !this.head ) {
-			this.head = node;
-		} else {
-			var lastNode = this.getLastNode();
-			lastNode.next = node;
-			lastNode.next.prev = lastNode;
-		}
-		this.count++;
-	};
-	
-	/*
-	 * Adds a node at the beginning of the list (becomes head)
-	 * @param {*} data The node data
-	 */
-	LList.prototype.unshift = function( data ) {
-		var node = createNode( data );
-		if ( !this.head ) {
-			this.head = node;
-		} else {
-			var headNodeCache = this.head;
-			this.head = node;
-			this.head.next = headNodeCache;
-			this.head.next.prev = this.head;
-		}
-		this.count++;
-	};
-	
 
-	/*
-	 * Remove all nodes with a given value
-	 * @param {*} search The value to match against the list
-	 * @param {Number} max
+	/**
+	 * Linked list class
 	 */
-	LList.prototype.remove = function( search, max ) {
-		max = ( typeof max !== "undefined" && max > 0 ) ? max : Number.MAX_VALUE;
-		this.forEach( function( node, index ) {
-			if ( node.data == search ) {
-				if ( !node.prev ) {
-					this.head = node.next;
-					this.head.prev = null;
+	class LList {
+
+		/**
+		 * constructor
+		 */
+		constructor() {
+			this.head = null;
+			this.count = 0;
+		}
+
+		/**
+		 * Create a new node object
+		 * @param {*|Null} value The node data
+		 * @return {Object} node
+		 */
+		createNode(data = null) {
+			return {
+				data: data === undefined ? null : data,
+				next: null,
+				prev: null
+			}
+		}
+
+		/**
+		 * Add nodes at the end of the list & increment the count
+		 * @param {*} data The node data
+		 */
+		push(...args) {
+			args.forEach(data => {
+				let _node = this.createNode(data);
+				this.count++;
+				if (!this.head) {
+					this.head = _node;
 				} else {
-					node.prev.next = node.next;
-					if ( node.next ) {
-						node.next.prev = node.prev;
-					}
+					let lastNode = this.getLastNode();
+					lastNode.next = _node;
+					lastNode.next.prev = lastNode;
 				}
-				this.count--;
-				if ( --max <= 0 ) return true; // break the loop
-			}
-		});
-	};
-	
-	/*
-	 * Remove the last node of the list and returns it
-	 * @return {Object|Null} The removed node or null
-	 */
-	LList.prototype.pop = function() {
-		var lastNode = this.getLastNode();
-		if ( lastNode ) {
-			lastNode.prev.next = null;
-			this.count--;
+			});
 		}
-		return lastNode;
-	};
-	
-	/*
-	 * Remove the first node of the list and returns it
-	 * @return {Object|Null} The removed node or null
-	 */
-	LList.prototype.shift = function() {
-		if ( !this.head ) return null;
-		var headNodeCache = this.head;
-		this.head = this.head.next;
-		if ( this.head ) {
-			this.head.prev = null;
-		}
-		this.count--;
-		return headNodeCache;
-	};
-	
-	/*
-	 * Merge the list with another list
-	 * @param {Object: List} list A LList object reference
-	 */
-	LList.prototype.concat = function( list ) {
-		if ( !LList.isList( list ) ) {
-			throw "LList::concat: invalid LList:list argument";
-		}
-		if ( !list.head ) return;
-		var lastNode = this.getLastNode();
-		if ( lastNode ) {
-			lastNode.next = list.head;
-			lastNode.next.prev = lastNode;
-			this.count += list.count;
-		} else {
-			this.head = list.head;
-			this.count = list.count;
-		}
-	};
-	
-	/*
-	 * Creates creates a new LList with all nodes that pass the test implemented by the provided function.
-	 * @param {Function} callback The filter function to test each node of the list return true to keep the node otherwise false
-	 * @param {Object} thisArg Optional to use as this when executing callback
-	 * @return {Object} A new LList
-	 */
-	LList.prototype.filter = function( callback, thisArg ) {
-		thisArg = ( thisArg && typeof thisArg === "object" ) ? thisArg : null;
-		var nList = new LList;
-		this.forEach(function( node, index ) {
-			if ( callback.call( thisArg, node, index ) ) {
-				nList.push( node.data );
-			}
-		});
-		return nList;
-	};
-	
-	/*
-	 * Returns the first node whose data matches
-	 * @param {*} data The search value to match a node's data
-	 * @return {Object|Null} A LList node or null
-	 */
-	LList.prototype.search = function( data ) {
-		return this.forEach( function( node ) {
-			if ( node.data == data ) return node;
-		}) || null;
-	};
-	
-	/*
-	 * Returns the node at the given list index
-	 * @param {Number} _index The node list index
-	 * @return {Object|Null} A LList node or null
-	 */
-	LList.prototype.searchIndex = function( _index ) {
-		return this.forEach( function( node, index ) {
-			if ( index == _index ) return node;
-		}) || null;
-	};
-	
-	/*
-	 * Reverse the list
-	 */
-	LList.prototype.reverse = function() {
-		var rList = new LList; 
-		this.forEach(function( node ) {
-			rList.unshift( node.data );
-			if ( node.prev ) {
-				node.prev.prev = null;
-				node.prev.next = null;
-			}
-		});
-		this.head = rList.head;
-	};
-	
-	/*
-	 * Sort the list ( a < b )
-	 */
-	LList.prototype.sort = function() {
-		this.forEach(function( node ) {
-			var prevNode = node.prev;
-			while( prevNode && node.data < prevNode.data ) {
-				var prevDataCache = prevNode.data;
-				prevNode.data = node.data;
-				node.data = prevDataCache;
-				node = prevNode;
-				prevNode = prevNode.prev;
-			}
-		});
-	};
-	
-	/*
-	 * checks if the the input arg is a valid LList reference
-	 * @param {Object: LList} list
-	 * @return {Boolean}
-	 */
-	LList.isList = function( list ) {
-		return ( Object.getPrototypeOf( list ) === LList.prototype ) ? true : false;
-	};
-	
-	if ( noGlobal ) {
-		// return LList for export
-		return LList;
-	}
-	
-	// global define LList
-	global.LList = LList;
-	
-});
 
+		/**
+		 * Add nodes at the head of the list & increment the count
+		 * @param {*} data The node data
+		 */
+		unshift(...args) {
+			args.forEach(data => {
+				let _node = this.createNode(data);
+				this.count++;
+				if (!this.head) {
+					this.head = _node;
+				} else {
+					let oldHead = Object.assign({}, this.head);
+					this.head = _node;
+					this.head.next = oldHead;
+					this.head.next.prev = this.head;
+				}
+			});
+		}
+
+		/**
+		 * Merge LLists
+		 * @param {Object} list A LList instance
+		 */
+		concat(llist) {
+			if (!llist instanceof LList) throw 'concat value is not an instance of LList';
+			if (!llist.head) return;
+			if (!this.head) {
+				this.head = llist.head;
+				this.count = llist.count;
+			} else {
+				let lastNode = this.getLastNode();
+				lastNode.next = llist.head;
+				lastNode.next.prev = lastNode;
+				this.count += llist.count;
+			}
+		};
+
+		/**
+		 * Get the last node in the list
+		 * @return {Object|Null} The last node in the list or null
+		 */
+		getLastNode(node = this.head) {
+			if (!node) return null;
+			while(node.next) {
+				node = node.next;
+			}
+			return node;
+		}
+
+		/**
+		 * Iterate over the list
+		 * and invoke the callback function for each node
+		 * @param {Function} callback function which gets called for each node
+		 * * providing the current node and index as arguments
+		 * * @args {Object} The current node
+		 * * @args {Number} The current node's list index
+		 * @param {Object} thisArg Optional argument to be accessible in
+		 * * the callback funcion as this else it will be the class instance
+		 * @throws Error if callback is not a function
+		 */
+		forEach(callback, thisArg = this, node = this.head) {
+			if (typeof callback !== 'function') throw 'callback must be a function';
+			if (!node) return;
+			let index = 0;
+			while(node) {
+				callback.call(thisArg, node, index++);
+				node = node.next;
+			}
+		}
+
+		/**
+		 * Returns a new filtered instance of the class
+		 * @param {Function} callback filter function which gets called for each node
+		 * * providing the current node and index as arguments
+		 * * if it returns true the node will be kept else not
+		 * * @args {Object} The current node
+		 * * @args {Number} The current node's list index
+		 * @param {Object} thisArg Optional argument to be accessible in
+		 * * the callback funcion as this else it will be the class instance
+		 * @return new LList instance
+		 * @throws Error if callback is not a function
+		 */
+		filter(callback, thisArg = this) {
+			if (typeof callback !== 'function') throw 'callback must be a function';
+			let filteredData = [];
+			this.forEach((node, index) => {
+				if (callback.call(this, node, index))
+					filteredData.push(node.data);
+			}, thisArg);
+			let newList = new LList;
+			newList.push(...filteredData);
+			return newList;
+		}
+
+		/**
+		 * Remove all nodes which data matches the search value
+		 * @param {*} search The search value
+		 * @param {Number} max maximum node deletions
+		 * @return {Number} removeCount the count of removed nodes
+		 */
+		remove(search, max = null, node = this.head) {
+			let removeCount = 0;
+			while(node) {
+				if (node.data === search) {
+					this.count--;
+					removeCount++;
+					if (!node.prev) {
+						this.head = node.next;
+						this.head.prev = null;
+					} else {
+						node.prev.next = node.next;
+						if (node.next)
+							node.next.prev = node.prev;
+					}
+					if (max && removeCount === max) break;
+				}
+				node = node.next;
+			}
+			return removeCount;
+		}
+
+		/**
+		 * Remove and return the last node of the list and decrement the node count
+		 * @return {Object|Null} The removed node or null
+		 */
+		pop() {
+			let lastNode = this.getLastNode();
+			if (lastNode) {
+				lastNode.prev.next = null;
+				this.count--;
+			}
+			lastNode.prev = null;
+			return lastNode;
+		}
+
+		/**
+		 * Remove and return the head node of the list and decrement the node count
+		 * @return {Object|Null} The removed node or null
+		 */
+		shift() {
+			if (!this.head) return null;
+			let headNode = this.head;
+			if (!this.head.next) {
+				this.head = null;
+			} else {
+				this.head = headNode.next;
+				this.head.prev = null;
+				headNode.next = null;
+			}
+			this.count--;
+			return headNode;
+		}
+
+		/**
+		 * Returns the first node whose data matches the search
+		 * @param {*} search search value
+		 * @param {Number} fromIndex the starting index
+		 * @param {Object} node the node to begin with
+		 * @return {Object|Null} node or null
+		 */
+		find(search, fromIndex = 0, node = this.head) {
+			let index = 0;
+			while(node) {
+				if (index >= fromIndex)
+					if (node.data === search)
+						return node;
+				node = node.next;
+				index++;
+			}
+			return null;
+		}
+
+		/**
+		 * Return the node with the given index
+		 * @param {Number} index the search index
+		 * @param {Number} fromIndex the starting index
+		 * @return {Object|Null} node or null
+		 */
+		findIndex(index, fromIndex = 0, node = this.head) {
+			let _index = 0;
+			while(node) {
+				if (_index >= fromIndex)
+					if ((_index - fromIndex) === index)
+						return node;
+				node = node.next;
+				_index++;
+			}
+			return null;
+		}
+
+		/**
+		 * Reverse the list
+		 */
+		reverse() {
+			var revList = new LList;
+			this.forEach((node) => {
+				revList.unshift(node.data);
+			});
+			this.head = revList.head;
+		}
+
+		/**
+		 * Sort the list
+		 * @param {Function} compFunc The compare function that defines the sort order
+		 */
+		sort(compFunc = (a,b) => a.data > b.data) {
+			this.forEach((node) => {
+				let prevNode = node.prev;
+				while(prevNode && compFunc(prevNode, node)) {
+					let prevNodeData = prevNode.data;
+					prevNode.data = node.data;
+					node.data = prevNodeData;
+					node = prevNode;
+					prevNode = prevNode.prev;
+				}
+			});
+		}
+
+	}
+
+	if (noGlobal) return LList;
+
+	global.LList = LList;
+
+});
